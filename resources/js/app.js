a7b275ae827102b8f9ec3e7d8584c405ec31a665
翟宇鑫 <zhaiyuxin103@hotmail.com>
2022-08-05 16:31:56 +0800

    build(npm): add font awesome

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,8 +6,15 @@ import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+
+library.add(fas, far, fab);
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -16,6 +23,7 @@ createInertiaApp({
         return createApp({ render: () => h(app, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
+            .component('font-awesome-icon', FontAwesomeIcon)
             .mount(el);
     },
 });
