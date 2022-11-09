b38a0e8c3911cdf916c05c6f91a65db69fd3239f
翟宇鑫 <zhaiyuxin103@hotmail.com>
2022-11-09 11:10:34 +0800

    feat(notifications): 支持队列的邮件通知

--- app/Notifications/TopicReplied.php
+++ app/Notifications/TopicReplied.php
@@ -4,10 +4,11 @@ namespace App\Notifications;
 
 use App\Models\Reply;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class TopicReplied extends Notification
+class TopicReplied extends Notification implements ShouldQueue
 {
     use Queueable;
 
@@ -43,10 +44,11 @@ class TopicReplied extends Notification
      */
     public function toMail(mixed $notifiable): MailMessage
     {
+        $uri = $this->reply->topic->link(['#reply-'.$this->reply->id]);
+
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+                    ->line('你的话题有新回复！')
+                    ->action('查看回复', $uri);
     }
 
     /**
