8653682109170d5b2c77125bd687cc57772e17f8
翟宇鑫 <zhaiyuxin103@hotmail.com>
2022-11-14 22:52:49 +0800

    feat(api): 消息通知列表

---
+++ app/Http/Controllers/Api/NotificationsController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\NotificationResource;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Jiannei\Response\Laravel\Support\Facades\Response;
+
+class NotificationsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param  Request  $request
+     * @return JsonResponse|JsonResource
+     */
+    public function index(Request $request): JsonResponse|JsonResource
+    {
+        $notifications = $request->user()->notifications()->paginate();
+
+        return Response::success(NotificationResource::collection($notifications));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  Request  $request
+     * @return JsonResponse|JsonResource
+     */
+    public function store(Request $request): JsonResponse|JsonResource
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return JsonResponse|JsonResource
+     */
+    public function show(int $id): JsonResponse|JsonResource
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  Request  $request
+     * @param  int  $id
+     * @return JsonResponse|JsonResource
+     */
+    public function update(Request $request, int $id): JsonResponse|JsonResource
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return JsonResponse|JsonResource
+     */
+    public function destroy(int $id): JsonResponse|JsonResource
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\AuthorizationsController;
 use App\Http\Controllers\Api\CaptchasController;
 use App\Http\Controllers\Api\CategoriesController;
 use App\Http\Controllers\Api\ImagesController;
+use App\Http\Controllers\Api\NotificationsController;
 use App\Http\Controllers\Api\RepliesController;
 use App\Http\Controllers\Api\TopicsController;
 use App\Http\Controllers\Api\UsersController;
@@ -84,6 +85,8 @@ Route::prefix('v1')
                     Route::apiResource('topics', TopicsController::class)->only(['store', 'update', 'destroy']);
                     // 发布、删除回复
                     Route::apiResource('topics.replies', RepliesController::class)->only(['store', 'destroy']);
+                    // 通知列表
+                    Route::apiResource('notifications', NotificationsController::class)->only(['index']);
                 });
             });
     });
